62252bad493e93b3daac35456fff454b722599f1
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-13 00:20:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,5 +9,12 @@ def countdown(number_of_seconds)
 end
 
 
-def countdown_with_sleep
+def countdown_with_sleep(number_of_seconds)
+  while number_of_seconds >0
+    puts "#{number_of_seconds} SECOND(S)"
+    sleep(1)
+    number_of_seconds -= 1 
+  end
+  "HAPPY NEW YEAR"
+    
 end
