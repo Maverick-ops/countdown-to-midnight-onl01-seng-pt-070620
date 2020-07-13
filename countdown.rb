ef9167237e903d8b2f5b467a1400429c7864c54e
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-13 00:04:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 
 def countdown(number_of_seconds)
-  while number_of_seconds > 0
+  while number_of_seconds < 10 
   puts "#{number_of_seconds} SECOND(S)!"
   number_of_seconds -= 1
   end 
