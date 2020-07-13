3cb4fa30493094bd355fe85f597b64a39dfbbdf1
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-13 00:01:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 #write your code here
 
-def countdown
+def countdown(number_of_seconds)
+  while number_of_seconds > 0
+  puts "#{number_of_seconds}SECOND(S)!"
+  number_of_seconds -= 1
+  end 
+  puts "HAPPY NEW YEAR!"
 end
